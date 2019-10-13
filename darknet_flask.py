94168710acf368bb246bd536995d02db36047821
Yiling Cao <yiling.cao@gmail.com>
2019-10-13 18:49:42 +0800

    add not loading neural net while development.

--- darknet_flask.py
+++ darknet_flask.py
@@ -40,7 +40,15 @@ UPLOAD_FOLDER = 'darknet_http_server/upload/'
 ALLOWED_EXTENSIONS = set(['bmp', 'tif', 'png', 'jpg', 'jpeg', 'gif'])
 approx_dist = True
 crop_person = True
+hasGPU = True
+LOAD_NNET=False
 
+netMain = None
+metaMain = None
+altNames = None
+
+#default threshold for detection
+default_thresh = 0.25
 
 
 def sample(probs):
@@ -72,7 +80,6 @@ class DETECTION(Structure):
                 ("objectness", c_float),
                 ("sort_class", c_int)]
 
-
 class IMAGE(Structure):
     _fields_ = [("w", c_int),
                 ("h", c_int),
@@ -83,134 +90,11 @@ class METADATA(Structure):
     _fields_ = [("classes", c_int),
                 ("names", POINTER(c_char_p))]
 
-
-#lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
-#lib = CDLL("libdarknet.so", RTLD_GLOBAL)
-hasGPU = True
-if os.name == "nt":
-    cwd = os.path.dirname(__file__)
-    os.environ['PATH'] = cwd + ';' + os.environ['PATH']
-    winGPUdll = os.path.join(cwd, "yolo_cpp_dll.dll")
-    winNoGPUdll = os.path.join(cwd, "yolo_cpp_dll_nogpu.dll")
-    envKeys = list()
-    for k, v in os.environ.items():
-        envKeys.append(k)
-    try:
-        try:
-            tmp = os.environ["FORCE_CPU"].lower()
-            if tmp in ["1", "true", "yes", "on"]:
-                raise ValueError("ForceCPU")
-            else:
-                print("Flag value '"+tmp+"' not forcing CPU mode")
-        except KeyError:
-            # We never set the flag
-            if 'CUDA_VISIBLE_DEVICES' in envKeys:
-                if int(os.environ['CUDA_VISIBLE_DEVICES']) < 0:
-                    raise ValueError("ForceCPU")
-            try:
-                global DARKNET_FORCE_CPU
-                if DARKNET_FORCE_CPU:
-                    raise ValueError("ForceCPU")
-            except NameError:
-                pass
-            # print(os.environ.keys())
-            # print("FORCE_CPU flag undefined, proceeding with GPU")
-        if not os.path.exists(winGPUdll):
-            raise ValueError("NoDLL")
-        lib = CDLL(winGPUdll, RTLD_GLOBAL)
-    except (KeyError, ValueError):
-        hasGPU = False
-        if os.path.exists(winNoGPUdll):
-            lib = CDLL(winNoGPUdll, RTLD_GLOBAL)
-            print("Notice: CPU-only mode")
-        else:
-            # Try the other way, in case no_gpu was
-            # compile but not renamed
-            lib = CDLL(winGPUdll, RTLD_GLOBAL)
-            print("Environment variables indicated a CPU run, but we didn't find `"+winNoGPUdll+"`. Trying a GPU run anyway.")
-else:
-    lib = CDLL("./libdarknet.so", RTLD_GLOBAL)
-lib.network_width.argtypes = [c_void_p]
-lib.network_width.restype = c_int
-lib.network_height.argtypes = [c_void_p]
-lib.network_height.restype = c_int
-
-copy_image_from_bytes = lib.copy_image_from_bytes
-copy_image_from_bytes.argtypes = [IMAGE,c_char_p]
-
 def network_width(net):
     return lib.network_width(net)
 
 def network_height(net):
     return lib.network_height(net)
-
-predict = lib.network_predict_ptr
-predict.argtypes = [c_void_p, POINTER(c_float)]
-predict.restype = POINTER(c_float)
-
-if hasGPU:
-    set_gpu = lib.cuda_set_device
-    set_gpu.argtypes = [c_int]
-
-make_image = lib.make_image
-make_image.argtypes = [c_int, c_int, c_int]
-make_image.restype = IMAGE
-
-get_network_boxes = lib.get_network_boxes
-get_network_boxes.argtypes = [c_void_p, c_int, c_int, c_float, c_float, POINTER(c_int), c_int, POINTER(c_int), c_int]
-get_network_boxes.restype = POINTER(DETECTION)
-
-make_network_boxes = lib.make_network_boxes
-make_network_boxes.argtypes = [c_void_p]
-make_network_boxes.restype = POINTER(DETECTION)
-
-free_detections = lib.free_detections
-free_detections.argtypes = [POINTER(DETECTION), c_int]
-
-free_ptrs = lib.free_ptrs
-free_ptrs.argtypes = [POINTER(c_void_p), c_int]
-
-network_predict = lib.network_predict_ptr
-network_predict.argtypes = [c_void_p, POINTER(c_float)]
-
-reset_rnn = lib.reset_rnn
-reset_rnn.argtypes = [c_void_p]
-
-load_net = lib.load_network
-load_net.argtypes = [c_char_p, c_char_p, c_int]
-load_net.restype = c_void_p
-
-load_net_custom = lib.load_network_custom
-load_net_custom.argtypes = [c_char_p, c_char_p, c_int, c_int]
-load_net_custom.restype = c_void_p
-
-do_nms_obj = lib.do_nms_obj
-do_nms_obj.argtypes = [POINTER(DETECTION), c_int, c_int, c_float]
-
-do_nms_sort = lib.do_nms_sort
-do_nms_sort.argtypes = [POINTER(DETECTION), c_int, c_int, c_float]
-
-free_image = lib.free_image
-free_image.argtypes = [IMAGE]
-
-letterbox_image = lib.letterbox_image
-letterbox_image.argtypes = [IMAGE, c_int, c_int]
-letterbox_image.restype = IMAGE
-
-load_meta = lib.get_metadata
-lib.get_metadata.argtypes = [c_char_p]
-lib.get_metadata.restype = METADATA
-
-load_image = lib.load_image_color
-load_image.argtypes = [c_char_p, c_int, c_int]
-load_image.restype = IMAGE
-
-rgbgr_image = lib.rgbgr_image
-rgbgr_image.argtypes = [IMAGE]
-
-predict_image = lib.network_predict_image
-predict_image.argtypes = [c_void_p, IMAGE]
-predict_image.restype = POINTER(c_float)
 
 def array_to_image(arr):
     import numpy as np
@@ -255,7 +139,7 @@ def detect_image(net, meta, im, thresh=.5, hier_thresh=.5, nms=.45, debug= False
     #custom_image = cv2.resize(custom_image,(lib.network_width(net), lib.network_height(net)), interpolation = cv2.INTER_LINEAR)
     #import scipy.misc
     #custom_image = scipy.misc.imread(image)
-    #im, arr = array_to_image(custom_image)		# you should comment line below: free_image(im)
+    #im, arr = array_to_image(custom_image)     # you should comment line below: free_image(im)
     num = c_int(0)
     if debug: print("Assigned num")
     pnum = pointer(num)
@@ -296,11 +180,6 @@ def detect_image(net, meta, im, thresh=.5, hier_thresh=.5, nms=.45, debug= False
     free_detections(dets, num)
     if debug: print("freed detections")
     return res
-
-
-netMain = None
-metaMain = None
-altNames = None
 
 def performDetect(imagePath="./data/dog.jpg", thresh= 0.25, configPath = "./cfg/yolov3.cfg", weightPath = "./yolov3.weights", metaPath= "./cfg/coco.data", showImage= False, makeImageOnly = False, initOnly= False):
     """
@@ -387,20 +266,97 @@ def performDetect(imagePath="./data/dog.jpg", thresh= 0.25, configPath = "./cfg/
         print("Initialized detector")
         return None
 
-
-
 #allowd upload files
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+if LOAD_NNET:
+    lib = CDLL("./libdarknet.so", RTLD_GLOBAL)
+    lib.network_width.argtypes = [c_void_p]
+    lib.network_width.restype = c_int
+    lib.network_height.argtypes = [c_void_p]
+    lib.network_height.restype = c_int
+
+    copy_image_from_bytes = lib.copy_image_from_bytes
+    copy_image_from_bytes.argtypes = [IMAGE,c_char_p]
+
+
+    predict = lib.network_predict_ptr
+    predict.argtypes = [c_void_p, POINTER(c_float)]
+    predict.restype = POINTER(c_float)
+
+    if hasGPU:
+        set_gpu = lib.cuda_set_device
+        set_gpu.argtypes = [c_int]
+
+    make_image = lib.make_image
+    make_image.argtypes = [c_int, c_int, c_int]
+    make_image.restype = IMAGE
+
+    get_network_boxes = lib.get_network_boxes
+    get_network_boxes.argtypes = [c_void_p, c_int, c_int, c_float, c_float, POINTER(c_int), c_int, POINTER(c_int), c_int]
+    get_network_boxes.restype = POINTER(DETECTION)
+
+    make_network_boxes = lib.make_network_boxes
+    make_network_boxes.argtypes = [c_void_p]
+    make_network_boxes.restype = POINTER(DETECTION)
+
+    free_detections = lib.free_detections
+    free_detections.argtypes = [POINTER(DETECTION), c_int]
+
+    free_ptrs = lib.free_ptrs
+    free_ptrs.argtypes = [POINTER(c_void_p), c_int]
+
+    network_predict = lib.network_predict_ptr
+    network_predict.argtypes = [c_void_p, POINTER(c_float)]
+
+    reset_rnn = lib.reset_rnn
+    reset_rnn.argtypes = [c_void_p]
+
+    load_net = lib.load_network
+    load_net.argtypes = [c_char_p, c_char_p, c_int]
+    load_net.restype = c_void_p
+
+    load_net_custom = lib.load_network_custom
+    load_net_custom.argtypes = [c_char_p, c_char_p, c_int, c_int]
+    load_net_custom.restype = c_void_p
+
+    do_nms_obj = lib.do_nms_obj
+    do_nms_obj.argtypes = [POINTER(DETECTION), c_int, c_int, c_float]
+
+    do_nms_sort = lib.do_nms_sort
+    do_nms_sort.argtypes = [POINTER(DETECTION), c_int, c_int, c_float]
+
+    free_image = lib.free_image
+    free_image.argtypes = [IMAGE]
+
+    letterbox_image = lib.letterbox_image
+    letterbox_image.argtypes = [IMAGE, c_int, c_int]
+    letterbox_image.restype = IMAGE
+
+    load_meta = lib.get_metadata
+    lib.get_metadata.argtypes = [c_char_p]
+    lib.get_metadata.restype = METADATA
+
+    load_image = lib.load_image_color
+    load_image.argtypes = [c_char_p, c_int, c_int]
+    load_image.restype = IMAGE
+
+    rgbgr_image = lib.rgbgr_image
+    rgbgr_image.argtypes = [IMAGE]
+
+    predict_image = lib.network_predict_image
+    predict_image.argtypes = [c_void_p, IMAGE]
+    predict_image.restype = POINTER(c_float)
 
 
 app = Flask(__name__)
 Bootstrap(app)
-performDetect()
-default_thresh = 0.25
+if LOAD_NNET:
+    performDetect()
+
 
 #testing if works!
 #imagePath="./data/dog.jpg"
@@ -409,13 +365,10 @@ default_thresh = 0.25
 def crop_person(img):
     return ""
 
-
 #root
 @app.route('/')
 def hello_world():
     return render_template('index.html', value=0)
-    #return '<h1>Artificial Neural Net Loaded.</h1><br><a href="/upload">upload a file to detection.</a>'
-
 
 @app.route('/api/health')
 def health():
